b335b50c9aa1bafeed37f969d769688fcd23a005
David-Bae <bellerin629@gmail.comclear>
2022-05-13 06:51:39 +0900

    윈도우 환경에서 확인해주세요

--- src/AutoTest.java
+++ src/AutoTest.java
@@ -3,6 +3,8 @@ import java.util.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.text.SimpleDateFormat;
+import java.awt.*;
+import javax.swing.*;
 
 class Bookmark {
 	private String name = new String();
@@ -106,9 +108,8 @@ class Bookmark {
 }
 
 class BookmarkList {
-	Bookmark[] bm = new Bookmark[100];
+	private ArrayList<Bookmark> list = new ArrayList<Bookmark>();
 	private int listNum = 0;
-	
 	
 	BookmarkList(String filename){
 		File file = new File(filename);			//create File instance of filename
@@ -128,42 +129,43 @@ class BookmarkList {
 				
 				Bookmark tmp = new Bookmark(inputLine.split("[,;]"));
 				
-				if(tmp.checkTimeFormat() && tmp.checkURL())
-					bm[listNum++] = tmp;
+				if(tmp.checkTimeFormat() && tmp.checkURL()) {
+					list.add(tmp);
+					listNum++;
+				}
 			}
 		}
 		
 		input.close();
 	}
 	
-	
 	public int numBookmarks() {								// inform number of bookmark list
 		return listNum;
 	}
 	
 	public Bookmark getBookmark(int i) {					// inform information of i'th bookmark
-		return bm[i];
+		return list.get(i);
 	}
-	
+
 	public void mergeByGroup() {
 		int sorted = 0;					//index of sorted
 
 		while(sorted != listNum - 2) {						//n-1번째 요소까지 정렬.
 			int find = sorted + 1;
 			
-			if(bm[sorted].getGroupName().equals(""))
+			if(list.get(sorted).getGroupName().equals(""))
 				sorted++;
 			else {
-				while(find != listNum && !(bm[sorted].getGroupName().equals(bm[find].getGroupName())))		//그룹이름이 같고, 인덱스가 가장 작은 것 구하기.
+				while(find != listNum && !(list.get(sorted).getGroupName().equals(list.get(find).getGroupName())))		//그룹이름이 같고, 인덱스가 가장 작은 것 구하기.
 					find++;
 				
 				if(find != listNum) {
 					for(int i = find; i > sorted + 1; i--) {
 						Bookmark tmp = new Bookmark();
 						
-						tmp = bm[i];
-						bm[i] = bm[i-1];
-						bm[i-1] = tmp;
+						tmp = list.get(i);
+						list.add(i, list.get(i-1));
+						list.add(i-1, tmp);
 					}
 				}
 			}
@@ -178,14 +180,82 @@ class BookmarkList {
 	
 }
 
-/*
+//GUI classes
+
+class BookmarkManagerFrame extends JFrame{
+	class MenuPanel extends JPanel{
+		MenuPanel(){
+			setLayout(new GridLayout(5,1));
+			add(new JButton("ADD"));
+			add(new JButton("DELETE"));
+			add(new JButton("UP"));
+			add(new JButton("DOWN"));
+			add(new JButton("SAVE"));
+		}
+	}
+	
+	BookmarkManagerFrame(BookmarkList list){
+		super("Bookmark Manager");
+		setLayout(new BorderLayout());
+		
+		BookmarkTable table = new BookmarkTable(toTable(list));
+		JScrollPane scrollPane = new JScrollPane(table);
+		
+		add(scrollPane, BorderLayout.CENTER);
+		add(new MenuPanel(), BorderLayout.EAST);
+		
+		pack();
+		setVisible(true);
+		setDefaultCloseOperation(super.EXIT_ON_CLOSE);
+	}
+	
+	static String[][] toTable(BookmarkList list){
+		String[][] contents = new String[list.numBookmarks()][6];
+		
+		for(int i = 0 ; i < list.numBookmarks() ; i++) {
+			String[] bookmarkContent = { 
+											"",
+											list.getBookmark(i).getGroupName(),
+											list.getBookmark(i).getName(),
+											list.getBookmark(i).getURL(),
+											list.getBookmark(i).getTime(),
+											list.getBookmark(i).getMemo()
+										};
+			contents[i] = bookmarkContent;
+		}
+			
+		return contents;
+	}
+	
+}
+
+class BookmarkTable extends JTable{
+	static String[] header = {"", "Group", "Name", "URL", "Created Time", "Memo" };
+	
+	BookmarkTable(String[][] contents){
+		super(contents, header);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
 public class AutoTest {
 	public static void main(String[] args) {
 		BookmarkList test = new BookmarkList("bigbang.txt");
+		test.mergeByGroup();
 		
 		for(int i =0; i< test.numBookmarks() ; i++) {
 			test.getBookmark(i).print();
 		}
+		
+		BookmarkManagerFrame aaaa = new BookmarkManagerFrame(test);
+		
 	}
 }
-*/
